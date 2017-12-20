22a71672980f6e656c813a2c806b9d51992fb9c9
Jeff Shannon <jtshannon@gmail.com>
2017-12-19 21:25:27 -0500

    More stuff

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
 (function() {
 
-  var app = angular.module("githubViewer", []);
+    var app = angular.module("githubViewer", []);
 
-  var MainController = function($scope, $http) {
+    var MainController = function(
+        $scope, github, $interval, 
+        $log, $anchorScroll, $location) {
 
-    var onUserComplete = function(response) {
-      $scope.user = response.data;
-      $http.get($scope.user.repos_url)
-           .then(onRepos, onError);
+        var onUserComplete = function(data) {
+            $scope.user = data;
+            github.getRepos($scope.user).then(onRepos, onError);
+        };
+
+        var onRepos = function(data) {
+            $scope.repos = data;
+            $location.hash("userDetails");
+            $anchorScroll();
+        };
+
+        var onError = function(reason) {
+            $scope.error = "Could not fetch the data.";
+        };
+
+        var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
+        };
+
+        var countdownInterval = null;
+        var startCountdown = function(){
+            countdownInterval = $interval(decrementCountdown, 1000, $scope.countdown);
+        };
+
+        $scope.search = function(username) {
+            $log.info("Searching for " + username);
+            github.getUser(username).then(onUserComplete, onError);
+            if(countdownInterval)    {
+                $interval.cancel(countdownInterval);
+                $scope.countdown = null;
+            }
+        };
+
+        $scope.username = "angular";
+        $scope.message = "GitHub Viewer";
+        $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
+
+
     };
 
-    var onRepos = function(response){
-      
-      $scope.repos = response.data;  
-        
-    };
-
-    var onError = function(reason) {
-      $scope.error = "Could not fetch the data.";
-    };
-
-
-    $scope.search = function(username){
-        $http.get("https://api.github.com/users/" + username)
-          .then(onUserComplete, onError);
-    };
-
-    $scope.username = "angular";
-    $scope.message = "GitHub Viewer";
-    $scope.repoSortOrder = "-stargazers_count";
-
-
-  };
-  
-  app.controller("MainController", ["$scope", "$http", MainController]);
+    app.controller("MainController", MainController);
 
 }());
